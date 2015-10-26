a62b63ebd69c426f7092ce1f94d46f9a5e0589b1
xxblancxx <xxblancxx@hotmail.com>
2015-10-26 09:21:38 +0100

    Can now pick tomatoplant - Animation and control works. no inventory yet

--- Assets/General Scripts/NetworkScript.cs
+++ Assets/General Scripts/NetworkScript.cs
@@ -54,5 +54,6 @@ public class NetworkScript : MonoBehaviour
         myPlayer.GetComponentInChildren<PlayerMovement>().enabled = true;
         myPlayer.GetComponentInChildren<CameraScript>().enabled = true;
         myPlayer.GetComponentInChildren<Camera>().enabled = true;
+        myPlayer.tag = "Player";
     }
 }
---
+++ Assets/objects/Scripts/TomatoScript.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TomatoScript : Photon.MonoBehaviour
+{
+    public bool isPicked = false;
+    private Animator anim;
+    private int pickTicker;
+    private int buttonFlicker;
+    // Use this for initialization
+    void Start()
+    {
+        buttonFlicker = 1;
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPicked)
+        {
+            anim.SetBool("IsPicked", true);
+        }
+        else
+        {
+            anim.SetBool("IsPicked", false);
+        }
+    }
+
+    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            stream.SendNext(isPicked);
+        }
+        if (stream.isReading)
+        {
+            isPicked = (bool)stream.ReceiveNext();
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (pickTicker >= 20 && !isPicked)
+        {
+            isPicked = true;
+        }
+        if (other.CompareTag("Player"))
+        { // This is our player
+           
+            if (Input.GetAxisRaw("Horizontal") == buttonFlicker && !isPicked)
+            {
+                Debug.Log(pickTicker);
+                pickTicker++;
+                buttonFlicker = buttonFlicker *-1;
+            }
+        }
+
+    }
+}
