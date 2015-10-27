ae204acfe11b56926fdab7f59d1bbf3cb025e6d3
TheCodeTinkerer <casper.svendsen@outlook.com>
2015-10-27 12:33:00 +0100

    Added scripts to objects
    - BaseItem
    - BaseStatItem
    - BaseWeapon

---
+++ Assets/objects/Scripts/BaseStatItem.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseStatItem : BaseItem
+{
+
+    private int stamina;
+    private int endurance;
+    private int strength;
+    private int intellect;
+
+    public int Stamina
+    {
+        get { return stamina; }
+        set { stamina = value; }
+    }
+
+    public int Enduarance
+    {
+        get { return endurance; }
+        set { endurance = value; }
+    }
+
+    public int Strength
+    {
+        get { return strength; }
+        set { strength = value; }
+    }
+
+    public int Intellect
+    {
+        get { return intellect; }
+        set { intellect = value; }
+    }
+}
---
+++ Assets/objects/Scripts/BaseWeapon.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseWeapon : BaseStatItem
+{
+
+    public enum WeaponTypes
+    {
+        SWORD,
+        STAFF,
+        DAGGER,
+        BOW,
+        SHIELD,
+        FOREARM
+    }
+
+    private WeaponTypes weaponType;
+    private int spellEffectID;
+
+    public WeaponTypes WeaponType
+    {
+        get { return weaponType; }
+        set { weaponType = value; }
+    }
+
+    public int SpellEffectID
+    {
+        get { return spellEffectID; }
+        set { spellEffectID = value; }
+    }
+}
