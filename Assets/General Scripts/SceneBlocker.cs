2480b1f623b3242700d51613472dcf455ab89ee2
xxblancxx <xxblancxx@hotmail.com>
2015-11-05 15:16:56 +0100

    SceneChange
    new scene

---
+++ Assets/General Scripts/SceneBlocker.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneBlocker : MonoBehaviour
+{
+    public string unlockOnQuest;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && other.GetComponent<PlayerInventory>().finishedQuestList.Contains(unlockOnQuest))
+        {
+            gameObject.SetActive(false);
+        }
+       
+       
+    }
+}
