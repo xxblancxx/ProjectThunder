7daf3ecc4aa359ed234720e1c4711b10383254de
xxblancxx <xxblancxx@hotmail.com>
2015-11-05 14:49:07 +0100

    In house music and outdoor music

--- Assets/General Scripts/DoorScript.cs
+++ Assets/General Scripts/DoorScript.cs
@@ -6,6 +6,7 @@ public class DoorScript : MonoBehaviour
 {
     public int sceneToChangeTo;
     public Transform destination;
+    public AudioClip desiredMusic;
     public float maxXPos;
     public float minXPos;
     public float maxYPos;
@@ -27,6 +28,11 @@ public class DoorScript : MonoBehaviour
         //Debug.Log(other.gameObject.name);
         other.transform.position = destination.position;
         // Boundary Control Objects.
+
+        // set music to different song
+        other.GetComponent<AudioSource>().Stop();
+        other.GetComponent<AudioSource>().clip = desiredMusic;
+        other.GetComponent<AudioSource>().Play();
 
         CameraScript cam = other.transform.parent.Find("Main Camera").GetComponent<CameraScript>();
         //Boundary Control
